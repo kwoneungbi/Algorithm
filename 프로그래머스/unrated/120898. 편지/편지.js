bec897f2e63df55b0bcd1d7adaf54e9904f979a8
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-10-31 17:20:25 +0900

    [level unrated] Title: 편지, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.js
@@ -0,0 +1,8 @@
+// solution #1
+const solution = (message) => message.length * 2;
+
+// solution #2
+// const solution = (message) => Array.from(message).length * 2;
+
+// solution #3
+// const solution = (message) =>  message.split('').length * 2;
