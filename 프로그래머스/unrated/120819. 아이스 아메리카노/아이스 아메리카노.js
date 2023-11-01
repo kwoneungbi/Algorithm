a927db44dd11909e62c6a09040c8cf0bb25831f0
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-01 13:20:58 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,21 @@
+// solution #1
+const solution = (money) => [~~(money/5500), money%5500];
+
+
+// solution #2
+// const solution = (money) => [Math.floor(money/5500), money%5500];
+
+
+// solution #3
+// const solution = (money) => [parseInt(money/5500), money%5500];
+
+
+// solution #4
+// const solution = (money) => {
+//     var answer = [];
+
+//     answer.push(parseInt(money/5500));
+//     answer.push(money%5500);
+
+//     return answer;
+// }
