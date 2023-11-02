76fe3fa1c36dead45da82bd3d4a9fbf90b245def
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-02 18:41:48 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,21 @@
+// solution #1
+const solution = (strlist) => strlist.map(x=>x.length);
+
+
+// solution #2
+// const solution = (strlist) => {
+//   var answer = [];
+//   for (let i = 0; i < strlist.length; i++) {
+//     answer.push(strlist[i].length);
+//   }
+//   return answer;
+// }
+
+// solution #3
+// const solution = (strlist) => {
+//     const answer = []
+//     for (const i of strlist) {
+//         answer.push(i.length)
+//     }
+//     return answer;
+// }
