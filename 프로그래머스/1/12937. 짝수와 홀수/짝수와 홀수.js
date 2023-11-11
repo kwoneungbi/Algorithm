b013a4e9c1268f6c45fdeb15bf0f820d2726edca
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-11 22:17:08 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,9 @@
+// solution #1
+const solution = (num) => num % 2 ? "Odd" : "Even";
+
+// solution #2
+// const solution = (num) => {
+//     if(num % 2) return "Odd";
+//     else return "Even";
+// }
+
