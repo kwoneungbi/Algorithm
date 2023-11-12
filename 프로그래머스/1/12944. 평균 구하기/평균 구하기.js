607539b5bbed901ddfdc65f2f189a5064c024791
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-12 12:15:11 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,12 @@
+// solution #1
+const solution = (arr) => arr.reduce((acc, curr) => acc + curr) / arr.length;
+
+// solution #2
+// const solution = (arr) => {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length; 
+// };
+
