0ce6f97017ddf120d4bc3e99312e4c1c4da5e983
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-12 12:50:53 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 12:41:24
+2023년 11월 0일 12:50:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,27 +1,21 @@
 // solution #1
-const solution = (s) => {
-    const pLength = s.match(/p/ig)
-    const yLength = s.match(/y/ig)
-    return (pLength === null) && (yLength === null) ? true : (pLength === null) || (yLength === null) ? false : pLength.length === yLength.length
-}
+const solution = (s) => 
+    s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
 
 // solution #2
 // const solution = (s) => {
-//     let s = s.toUpperCase;
-    
-//     return s.split('P').length == s.split('Y');
-// } 
-
-// function numPY(s){
-//     return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+//     const pLength = s.match(/p/ig)
+//     const yLength = s.match(/y/ig)
+//     return (pLength === null) && (yLength === null) ? true : (pLength === null) || (yLength === null) ? false : pLength.length === yLength.length
 // }
 
 
 // solution #3
-// function solution(s){
+// const solution = (s) => {
 //     const sLength = s.length
 //     const pCount = sLength - s.replace(/p/gi).length
 //     const yCount = sLength - s.replace(/y/gi).length
-
 //     return pCount == yCount
 // }
+
+
