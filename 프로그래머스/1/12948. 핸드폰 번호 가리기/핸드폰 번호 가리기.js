637357766aad7dbeb1ac7486aca05a4f1c15747d
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-14 19:05:12 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,17 @@
-const solution = (phone_number) => {
-    const numberLength = phone_number.length;
-    const a = phone_number.slice(0, numberLength-4)
-    return a.replace(/[0-9]/g,"*") + phone_number.slice(numberLength-4,numberLength)
-}
+// solution #1 생각의 흐름대로 작성
+// const solution = (phone_number) => {
+//     const numberLength = phone_number.length;
+//     const hide = phone_number.slice(0, numberLength-4)
+//     return hide.replace(/[0-9]/g,"*") + phone_number.slice(numberLength-4,numberLength)
+// }
 
+// solution #2
+// const solution = (phone_number) => phone_number.replace(/\d(?=\d{4})/g, "*");
 
+// solution #3
+// const solution = (phone_number) => 
+// "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+
+// solution #4
+const solution = phone_number => 
+[...phone_number].fill("*",0,phone_number.length-4).join("")
