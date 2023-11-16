832a176161b2d9d0fa44ee9c8840020a3fc42dcd
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-16 20:09:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,8 +1,7 @@
-// solution #1
-// num이 짝수라면 num / 2
-// num이 홀수라면 num * 3 + 1
+// num이 짝수라면 num / 2, num이 홀수라면 num * 3 + 1
 // 1이 될때 까지 반복하기, 500번 반복할때까지 1이 안되면 -1 반환하기
 
+// solution #1
 const solution = (num) => {
     let count = 0;
     for(let j = 0; j < 500; j +=1) {
@@ -12,3 +11,18 @@ const solution = (num) => {
     }
     return num === 1 ? count : -1;  
 }
+
+
+// solution #2
+// const solution = (num) => {
+//     let count = 0;
+//     while(num !== 1 && count !== 500) {
+//         num = num % 2 ? num * 3 + 1 : num / 2;
+//         count++;
+//   }
+//     return num == 1 ? count : -1;
+// }
+
+// solution #3
+// const solution = (num, count = 0) => 
+//     (num == 1) ? ((count >= 500) ? -1 : count) : solution((num % 2 == 0) ? num / 2 : (num * 3) + 1, ++count);
