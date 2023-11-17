1a1b8be174b1a61a62fd70305f9632e6e67e3b4c
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-17 22:47:13 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,5 +1,10 @@
 // 매개변수 a, b는 길이가 같은 정수 배열
+
 // solution #1
+const solution = (a, b) => a.reduce((acc, curr, i) => acc + curr * b[i],0);
+
+
+// solution #2
 // const solution = (a, b) => {
 //     let result = 0;
 //     for(let i = 0; i < a.length; i += 1) {
@@ -7,7 +12,3 @@
 //     }
 //     return result
 // }
-
-// solution #2
-const solution = (a, b) => a.reduce((acc, _, i) => acc + a[i] * b[i],0);
-
