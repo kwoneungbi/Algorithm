73137d87c6efebba15e7cc6931c22f781af80c09
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-20 12:29:09 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,7 +1,17 @@
 // count번째 이용하면 price * count로 금액 받기
 // count번 탔을때 money에서 얼마 모자라는지 리턴, 부족하지 않다면 0 리턴
 
+// solution #1
 const solution = (price, money, count) => {
-    const result = money - Array(count).fill(0).map((x, i)=> (i + 1) * price).reduce((acc, curr) => acc + curr,0)
+    const result = money - Array(count).fill(0).map((_, i) => ++i * price).reduce((acc, curr) => acc + curr, 0)
     return result <= 0 ? Math.abs(result) : 0;
 } 
+
+// solution #2
+// const solution = (price, money, count) => {
+//     let result = 0;
+//     for (let i = 1; i <= count; i++) {
+//         result += price * i;
+//     }
+//     return result >= money ? result - money : 0;
+// }
