18746d3736f7b22163494f50f9781de5f488fa1a
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-20 23:10:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 41.57 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,4 +1,15 @@
 // arr은 0~9까지의 원소로 이루어져있다.
 // 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 한다.
 
-const solution = (arr) => arr.filter((x,i)=> x !== arr[i+1])
+// solution #1
+const solution = (arr) => arr.filter((x,i)=> x !== arr[i + 1])
+
+// solution #2
+// const solution = (arr) => {
+//     let result = [];
+    
+//     for(let i = 0; i < arr.length; i += 1) {
+//         if(arr[i + 1] !== arr[i]) result.push(arr[i])
+//     }
+//     return result;
+// }
