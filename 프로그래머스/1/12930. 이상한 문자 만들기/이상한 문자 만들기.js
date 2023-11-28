8166d14cee24d5aff2defc3e866e1c1ba32b93ec
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-28 16:19:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,14 +1,17 @@
+// solution #1
 const solution = (s) => 
     s.split(" ").map(x => x.split("").map((y, i) => i % 2 ? y.toLowerCase() : y.toUpperCase() ).join("")).join(" ")
 
-// function solution(s) {
-//   return s
-//     .split(" ")
-//     .map((x) =>
-//        x.split("")
-//         .map((y, index) => (index % 2 == 0 ? y.toUpperCase() : y.toLowerCase()))
-//         .join("")
-//     )
-//     .join(" ");
-// }
+// solution #2
+// const solution = (s) => {
+//   var result = "";
 
+//   for(var word of s.split(" ")) {
+//     for(var i in word) {
+//       result += word[i][parseInt(i) % 2 == 0 ? "toUpperCase" : "toLowerCase"]();
+//     }
+//     result += " ";
+//   };
+
+//   return result.slice(0, -1);
+// }
