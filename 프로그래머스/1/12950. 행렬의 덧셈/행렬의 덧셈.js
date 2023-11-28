cbf5aedf77b2782a7e8bb2efaf9710aebcd113e1
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-11-28 15:31:41 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.63 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,3 +1,17 @@
 // arr1 과 arr2를 더해주는데 이때 같은 뎁스끼리 더해주기
 
-const solution = (arr1, arr2) => arr1.map((a, i)=> a.map((b, j) => b + arr2[i][j]))
+// solution #1
+const solution = (arr1, arr2) => arr1.map((a, i)=> a.map((b, j) => b + arr2[i][j]));
+
+// solution #2
+// const solution = (arr1, arr2) => {
+//     var result = [[]];
+    
+//     for (var i = 0; i < arr1.length; i += 1){
+//         result[i] =[];
+//         for(var j = 0; j < arr1[i].length; j += 1){
+//             result[i].push(arr1[i][j] + arr2[i][j]);
+//         }
+//     }
+//     return result;
+// }
