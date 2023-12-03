50288365fd129fac238035e480bb6447ef3920ec
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-03 18:35:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,1 +1,5 @@
-const solution = (n) => parseInt([...n.toString(3)].reverse().join(""), 3)
+// solution #1
+const solution = (n) => parseInt([...n.toString(3)].reverse().join(""), 3);
+
+// solution #2
+// const solution = (n) => parseInt((Array.from(n.toString(3)).reverse().join("")),3);
