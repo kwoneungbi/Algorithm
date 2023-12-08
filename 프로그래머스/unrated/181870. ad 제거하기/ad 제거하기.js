5c3595065cf041ca0ee9ec2549df2c8300419ead
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-08 22:51:48 +0900

    [level unrated] Title: ad 제거하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
@@ -1,3 +1,12 @@
-const solution = (strArr) => {
-    return strArr.filter(x => !x.includes("ad"))
-}
+// solution #1
+const solution = (strArr) => strArr.filter(x => !x.includes("ad"))
+
+
+// solution #2
+// const solution = (strArr) => {
+//     const answer = []
+//     strArr.forEach((a) => {
+//         a.includes("ad") ? a : answer.push(a)
+//     }) 
+//     return answer;
+// }
