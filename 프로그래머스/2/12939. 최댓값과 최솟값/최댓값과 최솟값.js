c479445a78fce5cfd418e88e418368978de4cd8d
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-10 20:47:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,11 @@
+// solution #1
+// const solution = (str) => {
+//     const string = str.split(" ").map(x => +x)
+//     return `${Math.min(...string)} ${Math.max(...string)}`
+// }
+
+// solution #2
 const solution = (str) => {
-    const string = str.split(" ").map(x => +x)
-    return `${Math.min(...string)} ${Math.max(...string)}`
+    const strSort = str.split(" ").sort((a,b) => a - b);
+    return `${strSort[0]} ${strSort.at(-1)}`
 }
