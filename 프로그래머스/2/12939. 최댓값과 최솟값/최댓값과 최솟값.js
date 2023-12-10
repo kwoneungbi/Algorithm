929b316967b1231d1529165b3efa1fe179f08c11
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-10 20:51:16 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,11 +1,17 @@
 // solution #1
-// const solution = (str) => {
-//     const string = str.split(" ").map(x => +x)
-//     return `${Math.min(...string)} ${Math.max(...string)}`
-// }
+const solution = (str) => {
+    const string = str.split(" ").map(x => +x)
+    return `${Math.min(...string)} ${Math.max(...string)}`
+}
 
 // solution #2
-const solution = (str) => {
-    const strSort = str.split(" ").sort((a,b) => a - b);
-    return `${strSort[0]} ${strSort.at(-1)}`
-}
+// const solution = (str) => {
+//     const strSort = str.split(" ").sort((a,b) => a - b);
+//     return `${strSort[0]} ${strSort.at(-1)}`
+// }
+
+// solution #3
+// const solution = (str) => {
+//     const strSort = str.split(" ").sort((a,b) => Number(a) - Number(b));    
+//     return `${strSort[0]} ${strSort.pop()}`;
+// }
