c2e3da266e4614b7825911e929ca94df19011141
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-13 22:28:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.82 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,19 @@
+// solution #1
+// const solution = (A, B) => {
+//     const sortA = A.sort((a, b) => a - b); // [1,2,4,5]
+//     const sortB = B.sort((a, b) => b - a); // [5,3,2]
+    
+//     return sortA.reduce((acc, curr, i) => acc + curr * sortB[i], 0);
+// }
+
+// solution #2
 const solution = (A, B) => {
     const sortA = A.sort((a, b) => a - b); // [1,2,4,5]
     const sortB = B.sort((a, b) => b - a); // [5,3,2]
+	let result = 0;
     
-    return sortA.reduce((acc, curr, i) => acc + curr * sortB[i], 0);
+	sortA.forEach((x, i) => {
+		result += x * sortB[i];
+    })
+	return result;
 }
-    
