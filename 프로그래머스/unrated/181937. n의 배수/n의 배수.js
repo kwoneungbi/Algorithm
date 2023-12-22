84506f8623972e0e709178400ca7efab5aee3a2f
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-22 23:54:19 +0900

    [level unrated] Title: n의 배수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.js
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.js
@@ -1,1 +1,11 @@
+// solution #1
 const solution = (num, n) => num % n ? 0 : 1
+
+// solution #2
+// const solution = (num, n) => {
+//     if(num % n) return 0;
+//     return 1;
+// }
+
+// solution #3
+// const solution = (m, n) => +(m % n === 0)
