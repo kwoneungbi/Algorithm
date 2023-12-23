51c6f607c9509d5babbe8f023542746e2de83807
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-23 23:19:08 +0900

    [level unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,8 @@
+// solution #1
+const solution = (a, b, flag) => flag ? a + b : a - b; 
+
+// solution #2
+// const solution = (a, b, flag) => {
+//     if(flag) return a + b;
+//     return a - b;
+// }
