1f242610f780c21693d25cfdbf13e05b65a2f1c8
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-25 14:44:10 +0900

    [level 2] Title: 숫자의 표현, Time: 1.70 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,7 +1,29 @@
-const solution = (n) => {
-    let result = 0; 
-    for(let i = 0; i <= n; i += 1 ) {
-        if(n % i === 0 && i % 2 === 1) result ++; // 약수이면서 홀수인 경우
+// const solution = (n) => {
+//     let result = 0; 
+//     for(let i = 0; i <= n; i += 1 ) {
+//         if(n % i === 0 && i % 2 === 1) result ++; // 약수이면서 홀수인 경우
+//     }
+//     return result;
+// } 
+
+const solution = n => {
+    let result = 0;
+    let sum = 0;
+    
+    for(let i = 1; i <= n; i += 1) {
+        for(let j = i; j <= n; j += 1) {
+            sum += j;
+            
+            if(sum === n) {
+                result += 1;
+                sum = 0;
+                break;
+            }
+            if(sum > n) {
+                sum = 0;
+                break;
+            }
+        }
     }
     return result;
-} 
+}
