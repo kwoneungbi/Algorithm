c2fa62d227eec0f51533f10fdab57c6416a78a1f
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2023-12-30 18:53:56 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,1 +1,11 @@
-const solution = (my_string, k) => my_string.repeat(k)
+// solution #1
+// const solution = (my_string, k) => my_string.repeat(k);
+
+// solution #2
+const solution = (my_string, k) => {
+    let result = ""
+    for(let i = 0; i < k; i += 1) {
+        result += my_string;
+    }
+    return result;
+}
