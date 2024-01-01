ed7bf63f9f6a7faa9ab58d1fa046d0e6c9ea0a58
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-01 22:19:42 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,6 +1,18 @@
+// solution #1
 const solution = (a, b) => {
     const strA = String(a);
     const strB = String(b);
     
     return +(strA + strB) > +(strB + strA) ? +(strA + strB) : +(strB + strA);
 }
+
+// solution #2
+// const solution = (a, b) => Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
+
+// solution #3
+// const solution = (a, b) => {
+//     let num1 = parseInt(""+a+b);
+//     let num2 = parseInt(""+b+a);
+    
+//     return num1 >= num2 ? num1 : num2;
+// }
