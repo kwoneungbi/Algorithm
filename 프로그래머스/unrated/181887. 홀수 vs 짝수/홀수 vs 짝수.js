3003070f00e034d235c468f1efb668ef46987758
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-09 20:48:18 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,3 +1,4 @@
+// solution #1
 const solution = num_list => {
     let a = 0;
     let b = 0;
@@ -7,3 +8,18 @@ const solution = num_list => {
     });
     return Math.max(a, b);
 }
+
+// solution #2
+// const solution = num_list => Math.max(...num_list.reduce(([x, y], c, i) => i % 2 ? [x + c, y] : [x, y + c], [0, 0]));
+
+// solution #3
+// const solution = num_list => {
+//     let a = 0;
+//     let b = 0;
+//     for(let i=0; i<num_list.length; i++){
+//         if(i%2) a += num_list[i];
+//         else b += num_list[i];
+//     }
+//     const arr = [a,b];
+//     return Math.max(...arr);
+// }
