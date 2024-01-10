da03be8e3db34960a72d2996ae00de910b4d3cd3
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-10 18:24:26 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,7 +1,11 @@
-const solution = (num_list, n) => {
-    const result = [];
-    for(let i = 0; i < num_list.length; i += n) {
-        result.push(num_list[i]);
-    }
-    return result;
-}
+// solution #1
+// const solution = (num_list, n) => {
+//     const result = [];
+//     for(let i = 0; i < num_list.length; i += n) {
+//         result.push(num_list[i]);
+//     }
+//     return result;
+// }
+
+// solution #2
+const solution = (num_list, n) => num_list.filter((_, i) => !(i % n));
