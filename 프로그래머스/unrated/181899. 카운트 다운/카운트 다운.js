13c5a6f71b47d32db40098fb296ce18fb0ac29cd
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-11 01:33:52 +0900

    [level unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,11 +1,11 @@
 // solution #1
-// const solution = (start_num, end_num) => {
-//     let result = [];
-//     for(let i = start_num; i >= end_num; i -= 1) {
-//         result.push(i);
-//     }
-//     return result;
-// }
+const solution = (start_num, end_num) => {
+    let result = [];
+    for(let i = start_num; i >= end_num; i -= 1) {
+        result.push(i);
+    }
+    return result;
+}
 
 // solution #2
-const solution = (start_num, end_num) => Array.from(Array(start_num - end_num + 1), (_, i) => start_num - i);
+// const solution = (start_num, end_num) => Array.from(Array(start_num - end_num + 1), (_, i) => start_num - i);
