837120683e76c7a7fd8d08c64ece9bc01c51bf34
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-11 23:14:57 +0900

    [level Lv.0] Title: 정수 부분, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181850. 정수 부분/정수 부분.js
@@ -0,0 +1,11 @@
+// solution #1
+// const solution = flo => Math.floor(flo);
+
+// solution #2
+// const solution = flo => ~~flo;
+
+// solution #3
+// const solution = flo => Math.trunc(flo);
+
+// solution #4
+const solution = flo => parseInt(flo);
