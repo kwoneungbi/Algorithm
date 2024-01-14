6c6042e4215c3b00f24cdda3a051e2eefd594d54
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-14 16:24:06 +0900

    [level Lv.1] Title: 시저 암호, Time: 4.23 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.js
@@ -3,6 +3,7 @@
 // 1. s가 알파벳인지 숫자인지 확인하기
 // 2. n만큼 민 문자열을 반환하기
 
+// solution #1
 const solution = (s, n) => {
     const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lower = "abcdefghijklmnopqrstuvwxyz";
@@ -21,3 +22,14 @@ const solution = (s, n) => {
     }
     return answer;
 }
+
+// solution #2
+// const solution = (s, n) => {
+//     const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//     const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+//     return s.split(' ').map((word) => word.split('').map((char) => {
+//     if (lower.indexOf(char) > -1) return lower[(lower.indexOf(char) + n) % lower.length]
+//     else if (upper.indexOf(char) > -1) return upper[(upper.indexOf(char) + n) % upper.length]
+//   }).join('')).join(' ')
+// }
