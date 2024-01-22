ed18b3b97fa77511d931a56f551de4d9f0f87fc1
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-22 13:06:23 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 3.57 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,1 +1,26 @@
+// solution #1
 const solution = (arr) => arr.map(x => x >= 50 && x % 2 === 0 ? x / 2 : x < 50 && x % 2 === 1 ? x * 2 : x); 
+
+// solution #2
+// const solution = (arr) => {
+//     const result = [];
+//     for(let i = 0; i < arr.length; i += 1) {
+//         const num = arr[i]
+//         if(num >= 50 && num % 2 === 0) result.push(num / 2);
+//         else if(num < 50 && num % 2 === 1) result.push(num * 2);
+//         else result.push(num);
+        
+//     }
+//     return result;
+// }
+
+// solution #3
+// const solution = (arr) => {
+//     const result = [];
+//     for(let i of arr) {
+//         if(i >= 50 && i % 2 === 0) result.push(i / 2);
+//         else if(i < 50 && i % 2 === 1) result.push(i * 2);
+//         else result.push(i);   
+//     }
+//     return result;
+// }
