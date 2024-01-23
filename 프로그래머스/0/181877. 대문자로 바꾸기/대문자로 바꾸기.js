372f8c4a5049c197d8eeeb48500a5528efd071e2
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-23 19:14:50 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,1 +1,14 @@
-const solution = (myString) => [...myString].map(x => x.toUpperCase()).join('');
+// solution #1
+// const solution = (myString) => [...myString].map(x => x.toUpperCase()).join('');
+
+// solution #2
+// const solution = (myString) => {
+//     let result = "";
+//     for(let i = 0; i < myString.length; i += 1) {
+//         result += myString[i].toUpperCase();
+//     } 
+//     return result;
+// }
+
+// solution #3
+const solution = (myString) => myString.toUpperCase();
