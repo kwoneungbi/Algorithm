f349c2a57999cf5b3d393c5b220a110b09a8df6c
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-25 11:37:20 +0900

    [level 0] Title: 문자열로 변환, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
@@ -3,3 +3,12 @@ const solution = (n) => String(n);
 
 // solution #2
 // const solution = (n) => `${n}` 
+
+// solution #3
+// const solution = (n) => n.toString();
+
+// solution #4
+// const solution = (n) => n + '';
+
+// solution #5
+// const solution = String;
