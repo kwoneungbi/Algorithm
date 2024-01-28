eb27d78329ca2ac0abc4cf79223da6e06debbb05
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-01-28 20:30:47 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,1 +1,14 @@
-const solution = (num_list) => num_list.reduce((acc, curr) => acc * curr) < num_list.reduce((acc, curr) => acc + curr) ** 2 ? 1 : 0;
+// solution 1
+// const solution = (num_list) => num_list.reduce((acc, curr) => acc * curr) < num_list.reduce((acc, curr) => acc + curr) ** 2 ? 1 : 0;
+
+// solution #2
+const solution = (num_list) => {
+    let accMul = 1;
+    let accSum = 0;
+    
+    for(const num of num_list) {
+        accMul *= num;
+        accSum += num;
+    }
+    return accMul < accSum ** 2 ? 1 : 0;
+}
