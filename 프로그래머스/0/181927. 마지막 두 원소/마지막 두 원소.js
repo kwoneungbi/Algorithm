6d4214420b7e76c2a4a1622a83a5e39d64395b3a
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-01 22:19:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,15 @@
-const solution = num_list => {
-    const last = num_list[num_list.length-1];
-    const lastone = num_list[num_list.length-2];
+// solution #1
+// const solution = num_list => {
+//     const last = num_list[num_list.length-1];
+//     const lastone = num_list[num_list.length-2];
     
-    last > lastone ? num_list.push(last - lastone) : num_list.push(last * 2);
+//     last > lastone ? num_list.push(last - lastone) : num_list.push(last * 2);
+//     return num_list;
+// }
+
+// solution #2
+const solution = (num_list) => {
+    const [sec, last] = num_list.slice(-2);
+    last > sec ? num_list.push(last - sec) : num_list.push(last * 2);
     return num_list;
 }
