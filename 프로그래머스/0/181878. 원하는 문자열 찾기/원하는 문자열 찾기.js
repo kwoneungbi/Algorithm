ceeb59d2b8af4587c73967d478d676e6bf13fe06
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-09 17:06:21 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.14 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,9 +1,14 @@
 // solution #1
-const solution = (myString, pat) => myString.toUpperCase().includes(pat.toUpperCase()) ? 1 : 0;
-
+// const solution = (myString, pat) => myString.toUpperCase().includes(pat.toUpperCase()) ? 1 : 0;
 
 // solution #2
 // const solution = (myString, pat) => myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 
 // solution #3
 // const solution = (myString, pat) => new RegExp(pat, 'i').test(myString) ? 1 : 0;
+
+// solution #4
+const solution = (myString, pat) => {
+    const [upperStr, upperPat] = [myString.toUpperCase(), pat.toUpperCase()];
+    return upperStr.includes(upperPat) ? 1 : 0;
+}
