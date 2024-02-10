121d6f5dca392e4ef66df006f9e5c921eb744757
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-10 15:26:43 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,7 +1,14 @@
-const solution = (my_string, is_prefix) => {
-    if(my_string[0] !== is_prefix[0]) return 0;
-    for(let i = 1; i < is_prefix.length; i += 1) {
-            if(my_string[i] !== is_prefix[i]) return 0;
-    }
-    return 1;
-}
+// solution #1
+// const solution = (my_string, is_prefix) => {
+//     if(my_string[0] !== is_prefix[0]) return 0;
+//     for(let i = 1; i < is_prefix.length; i += 1) {
+//             if(my_string[i] !== is_prefix[i]) return 0;
+//     }
+//     return 1;
+// }
+
+// solution #2
+// const solution = (my_string, is_prefix) => +my_string.startsWith(is_prefix);
+
+// solution #3
+const solution = (my_string, is_prefix) => my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0;
