cfc862fd10acf6f9ec7da86c98744a917d12f642
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-12 21:42:48 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.js
@@ -1,1 +1,11 @@
-const solution = (str1, str2) => str2.includes(str1) ? 1 : 0;
+// solution #1
+// const solution = (str1, str2) => str2.includes(str1) ? 1 : 0;
+
+// solution #2
+// const solution = (str1, str2) => +str2.includes(str1);
+
+// solution #3
+// const solution = (str1, str2) => RegExp(str1).test(str2) ? 1 : 0;
+
+// solution #4
+const solution = (str1, str2) => str2.split(str1).length > 1 ? 1 : 0;
