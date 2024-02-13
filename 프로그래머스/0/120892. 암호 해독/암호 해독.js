a06fa40a2981a3fe43fc77cd550780797e840510
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-13 19:56:42 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,1 +1,12 @@
-const solution = (cipher, code) => [...cipher].filter((x, i) => (i + 1) % code === 0).join("");
+// solution #1
+// const solution = (cipher, code) => [...cipher].filter((x, i) => (i + 1) % code === 0).join("");
+
+// solution #1
+const solution = (cipher, code) => {
+    let result = '';
+    
+    for(let i = code - 1; i < cipher.length; i += code) {
+        result += cipher[i];
+    }
+    return result;
+}
