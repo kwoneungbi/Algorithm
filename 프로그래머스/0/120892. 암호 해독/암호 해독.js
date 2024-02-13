fb11aed83b5466b053f07e5f1b5022c82a0becfd
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-13 19:59:25 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,12 +1,22 @@
 // solution #1
-// const solution = (cipher, code) => [...cipher].filter((x, i) => (i + 1) % code === 0).join("");
+const solution = (cipher, code) => [...cipher].filter((_, i) => (i + 1) % code === 0).join("");
 
 // solution #1
-const solution = (cipher, code) => {
-    let result = '';
+// const solution = (cipher, code) => {
+//     let result = "";
     
-    for(let i = code - 1; i < cipher.length; i += code) {
-        result += cipher[i];
-    }
-    return result;
-}
+//     for(let i = code - 1; i < cipher.length; i += code) {
+//         result += cipher[i];
+//     }
+//     return result;
+// }
+
+// solution #3
+// const solution = (cipher, code) => {
+//     let result = "";
+    
+//     cipher.split("").forEach((x, i) => {
+//         if((i + 1) % code === 0) result += x;
+//     })
+//     return result;
+// }
