959330d6c230ee4cf432c60cf0de24fa07fa8010
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-15 19:00:13 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,1 +1,5 @@
-const solution = (my_string, index_list) => index_list.map(x => my_string[x]).join("");
+// solution #1
+// const solution = (my_string, index_list) => index_list.map(x => my_string[x]).join("");
+
+// solution #2
+const solution = (my_string, index_list) => index_list.reduce((acc, i) => acc + my_string[i], '');
