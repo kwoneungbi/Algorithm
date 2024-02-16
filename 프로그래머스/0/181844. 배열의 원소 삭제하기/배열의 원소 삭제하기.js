3e10dee897ae02ef9eabfc5a3ecfcf36adc54a63
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-16 20:58:18 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,1 +1,14 @@
-const solution = (arr, delete_list) => arr.filter(x => !delete_list.includes(x));
+// solution #1
+// const solution = (arr, delete_list) => arr.filter(x => !delete_list.includes(x));
+
+// solution #2
+function solution(arr, delete_list) {
+    const result = [];
+    
+    for (let i = 0; i < arr.length; i += 1) {
+        if (!delete_list.includes(arr[i])) {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
