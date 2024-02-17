1a95c80c773b6913cd434c73c2fe0071e7272083
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-17 23:25:01 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,1 +1,18 @@
-const solution = (my_string) => [...my_string].map(x => /[a-z]/.test(x) ? x.toUpperCase() : x.toLowerCase()).join("");
+// solution #1
+// const solution = (my_string) => [...my_string].map(x => /[a-z]/.test(x) ? x.toUpperCase() : x.toLowerCase()).join("");
+
+// solution #2
+// const solution = (my_string) => [...my_string].map(x => x === x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join("");
+
+// solution #3
+const solution = (my_string) => {
+    let result = ''
+    for(let i of my_string){
+        if(i === i.toUpperCase()){
+            result += i.toLowerCase();
+        }else{
+            result += i.toUpperCase();
+        }
+    }
+    return result;
+}
