bce7689a130bdc1e95bd49d1a7a374117fef76ba
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-19 19:15:16 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,1 +1,5 @@
-const solution = (myString) => [...myString].map(x => x === 'a' || x === 'A' ? 'A' : x.toLowerCase()).join("");
+// solution. #1
+// const solution = (myString) => [...myString].map(x => x === 'a' || x === 'A' ? 'A' : x.toLowerCase()).join("");
+
+// solution #2
+const solution = (myString) => myString.toLowerCase().replaceAll('a','A');
