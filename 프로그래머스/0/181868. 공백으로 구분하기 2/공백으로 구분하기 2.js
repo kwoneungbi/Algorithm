a8246359f4060326b8251eb75577ebb09699c91c
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-20 10:05:27 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,1 +1,15 @@
-const solution = (my_string) => my_string.split(" ").filter(x => x !== "");
+// solution #1
+// const solution = (my_string) => my_string.split(" ").filter(x => x !== "");
+
+// solution #2
+const solution = (my_string) => {
+    let result = [];
+    let arr = my_string.split(" ");
+    
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] !== ""){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
