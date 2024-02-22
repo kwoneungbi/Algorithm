04c9875e28b3fec5d9408cd93b4a7536742d6943
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-22 12:17:55 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,3 +1,18 @@
+// solution #1
+// function solution(num_str) {
+//     return num_str.split("").reduce((acc,curr) => acc + +curr, 0);
+// }
+
+// solution #2
+// const solution = (num_str) => [...num_str].reduce((acc,curr) => acc + +curr, 0);
+
+// solution #3
 function solution(num_str) {
-    return num_str.split("").reduce((acc,curr) => acc + +curr, 0);
+    let sum = 0;
+    let newArr = num_str.split("").map(Number);
+    
+    for (let i = 0; i < newArr.length; i++) {
+        sum += newArr[i];
+    }
+    return sum;
 }
