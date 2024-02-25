74c49363ed966305ec2c675443872601bb8ede82
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-25 22:46:01 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.44 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,2 +1,9 @@
-const solution = (arr, intervals) => intervals.map(([a,b]) => arr.slice(a,b+1)).flat();
+// solution #1
+// const solution = (arr, intervals) => intervals.map(([a, b]) => arr.slice(a, b+1)).flat();
 
+// solution #2
+const solution = (arr, intervals) => {
+    const [[a,b],[c,d]] = intervals;
+
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
+}
