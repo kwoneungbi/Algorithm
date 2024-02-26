38f798d44ae3d3decf6ecc4192e3b02acbc7b1ee
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-02-26 19:22:32 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,6 @@
+const solution = (my_string, num1, num2) => {
+    my_string = my_string.split('');
+    
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
+}
