01b7f7d498168777dc22efcbfc60d6ad468557b1
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-03 20:49:50 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,1 +1,14 @@
-const solution = (str_list, ex) => str_list.filter(x => !x.includes(ex)).join("");
+// solution #1
+// const solution = (str_list, ex) => str_list.filter(x => !x.includes(ex)).join("");
+
+// solution #2
+const solution = (str_list, ex) => {
+    var answer = '';
+    let arr = []
+    for(x of str_list){
+        if(!x.includes(ex)){
+            arr.push(x)
+        }
+    }
+    return arr.join('');
+}
