77e87cb20da230db2bc8ab058c7ac5a0aa47d1b0
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-06 18:17:46 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,8 +1,19 @@
-const solution = (a, b) => {
-    const aa = a % 2;
-    const bb = b % 2;
+// solution #1
+// const solution = (a, b) => {
+//     const aa = a % 2;
+//     const bb = b % 2;
     
-    if(aa === 1 && bb === 1) return a*a + b*b;
-    if(aa === 1 || bb === 1) return 2 * (a + b);
-    else return Math.abs(a - b);
+//     if(aa === 1 && bb === 1) return a*a + b*b;
+//     if(aa === 1 || bb === 1) return 2 * (a + b);
+//     else return Math.abs(a - b);
+// }
+
+// solution #2
+const solution = (a, b) => {
+    const aa = a % 2 === 1;
+    const bb = b % 2 === 1;
+
+    if(aa && bb) return Math.pow(a, 2) + Math.pow(b, 2);
+    if(!aa && !bb) return Math.abs(a - b);
+    return 2 * (a + b);
 }
