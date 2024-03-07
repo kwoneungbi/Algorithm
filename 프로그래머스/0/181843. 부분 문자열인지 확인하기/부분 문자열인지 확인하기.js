a9afa7ae27cd2bf724e4f6b2d227f66f9706fc08
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-07 17:55:17 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,1 +1,5 @@
-const solution = (my_string, target) => +my_string.includes(target);
+// solution #1
+// const solution = (my_string, target) => +my_string.includes(target);
+
+// solution #2
+const solution = (my_string, target) => my_string.match(target) ? 1 : 0;
