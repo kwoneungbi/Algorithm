759c27afc84f7d3e0dae580e0bb8569cbaae8a79
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-09 23:46:09 +0900

    [level 0] Title: x 사이의 개수, Time: 4.26 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,1 +1,14 @@
-const solution = (myString) => myString.split('x').map(x => x.length);
+// solution #1
+// const solution = (myString) => myString.split('x').map(x => x.length);
+
+// solution #2
+const solution = (myString) => {
+    const nums = myString.split("x");
+    const result = [];
+
+    for (const word of nums) {
+        result.push(word.length);
+    }
+
+    return result;
+}
