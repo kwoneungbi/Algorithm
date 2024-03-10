6ebed7242763c92a0406eb78bcdda0fcba67efa6
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-10 17:27:23 +0900

    [level 0] Title: l로 만들기, Time: 5.86 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,9 @@
-const solution = (myString) => {
-    const alphabet = 'abcdefghijk';
+// solution #1
+// const solution = (myString) => {
+//     const alphabet = 'abcdefghijk';
     
-    return [...myString].map(x => [...alphabet].includes(x) ? 'l' : x).join("");
-}
+//     return [...myString].map(x => [...alphabet].includes(x) ? 'l' : x).join("");
+// }
+
+// solution #2
+const solution = (myString) => myString.replace(/[a-k]/g,'l')
