7e3217551c54797c6e0f4c13804a7dc2ec633b68
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-11 23:13:33 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,1 +1,12 @@
-const solution = (my_strings, parts) => parts.map(([x,y], idx) => my_strings[idx].slice(x, y+1)).join("")
+// solution #1
+// const solution = (my_strings, parts) => parts.map(([x,y], idx) => my_strings[idx].slice(x, y+1)).join("");
+
+// solution #2
+const solution = (my_strings, parts) => {
+    let result = '';
+    parts.forEach((part, i)=>{
+        const [s,e] = part;
+        result += my_strings[i].slice(s,e+1);
+    })
+    return result;
+}
