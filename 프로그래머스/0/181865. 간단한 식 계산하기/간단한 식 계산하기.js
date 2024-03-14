84709f26a57f31d5655b1434e7e1944b4ab463f0
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-14 18:02:13 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,1 +1,8 @@
-const solution = (binomial) => eval(binomial);
+// solution #1
+// const solution = (binomial) => eval(binomial);
+
+// solution #2
+const solution = (binomial) => {
+    const [a, ex, b] = binomial.split(" ");    
+    return (ex === "+")? +a + +b : (ex === "-")? a - b : a * b;
+}
