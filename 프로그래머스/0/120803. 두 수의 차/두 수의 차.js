6076d3e1bcc8d6ad9da0f0289e40adb734c4351b
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-15 09:01:47 +0900

    [level 0] Title: 두 수의 차, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
@@ -1,2 +1,10 @@
 // solution #1
-const solution = (num1, num2) => num1 - num2; 
+// const solution = (num1, num2) => num1 - num2; 
+
+// solution #2
+const solution = (num1, num2) => {
+    let result = 0;
+    result += num1;
+    result -= num2;
+    return result;
+}
