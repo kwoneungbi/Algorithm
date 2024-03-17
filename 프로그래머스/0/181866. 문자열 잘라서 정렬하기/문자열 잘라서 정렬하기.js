4e63f4fc3e7ec219f698f21a90e1d47f00c371ab
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-17 23:56:41 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 5.78 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -1,1 +1,6 @@
-const solution = (myString) => myString.split("x").filter(x => x !== "").sort();
+// solution #1
+// const solution = (myString) => myString.split("x").filter(x => x !== "").sort();
+
+// solution #2
+const solution = (myString) => myString.match(/[^x]+/g).sort();
+
