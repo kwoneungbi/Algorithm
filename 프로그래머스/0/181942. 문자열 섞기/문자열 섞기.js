efe4fdc0bdbcd236d05353cd10f3420dfd6a89b1
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-20 17:56:40 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,1 +1,12 @@
-const solution = (str1, str2) => [...str1].map((x, i) => `${x}${[...str2][i]}`).join("");
+// solution #1
+// const solution = (str1, str2) => [...str1].map((x, i) => `${x}${[...str2][i]}`).join("");
+
+// solution #2
+const solution = (str1, str2) => {
+    let result="";
+    
+    for(let i = 0 ; i < str1.length ; i++)
+        result+= str1[i]+str2[i];
+
+    return result;
+}
