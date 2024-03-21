58968708e71a9575eba3a81e546be5013a8a05f4
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-21 17:25:23 +0900

    [level 0] Title: 약수 구하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,15 @@
-const solution = (n) => {
-    let num = n;
-    let result = [];
-    let index = 1;
+// solution #1
+// const solution = (n) => {
+//     let num = n;
+//     let result = [];
+//     let index = 1;
     
-    while (index <= num) {
-      if (num % index === 0) result.push(index)
-      index++
-    }
-    return result;
-} 
+//     while (index <= num) {
+//       if (num % index === 0) result.push(index)
+//       index++
+//     }
+//     return result;
+// } 
+
+// solution #2
+const solution = (n) => Array(n).fill(0).map((v, index) => v + index + 1).filter((v) => n % v === 0);
