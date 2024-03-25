fe38f517bc794af5a3c4c898d2069643b01dc619
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-25 17:56:58 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,1 +1,12 @@
-const solution = (number) => [...number].reduce((acc, curr) => acc + +curr,0) % 9;
+// solution #1
+// const solution = (number) => [...number].reduce((acc, curr) => acc + +curr,0) % 9;
+
+// solution #2
+const solution = (number) => {
+    let result = 0;
+
+    for(let i of number){
+        result += Number(i);
+    }
+    return result % 9;
+}
