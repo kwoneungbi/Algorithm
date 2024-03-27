e58c5bdf6670c6d4846f4ca9aae534e91be99c91
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-27 17:47:42 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -2,14 +2,21 @@
 // const solution = (my_string, indices) => [...my_string].filter((x, i) => !indices.includes(i)).join("");
 
 // solution #2
+// const solution = (my_string, indices) => {
+//     var result = '';
+
+//     for(let i = 0; i < my_string.length; i += 1){
+//         if(!indices.includes(i)){
+//             result += my_string[i];
+//         }
+//     }
+
+//     return result;
+// }
+
+// solution #3
 const solution = (my_string, indices) => {
-    var result = '';
-
-    for(let i = 0; i < my_string.length; i += 1){
-        if(!indices.includes(i)){
-            result += my_string[i];
-        }
-    }
-
-    return result;
+  const set = new Set(indices);
+    
+  return [...my_string].filter((_, i) => !set.has(i)).join('');
 }
