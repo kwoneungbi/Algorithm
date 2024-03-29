9f151b080034666c814bf3ed45728e8bf3cf497b
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-29 20:13:04 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,34 @@
 // solution #1
+// const solution = (cards1, cards2, goal) => {
+//     for(const s of goal) {
+//         if(cards1[0] === s) {
+//             cards1.shift();
+//         } else if(cards2[0] === s) {
+//             cards2.shift();
+//         } else {
+//             return "No"
+//         }
+//     }
+//     return "Yes";
+// }
+
+// solution 2
 const solution = (cards1, cards2, goal) => {
-    for(const s of goal) {
-        if(cards1[0] === s) {
-            cards1.shift();
-        } else if(cards2[0] === s) {
-            cards2.shift();
-        } else {
-            return "No"
-        }
+    let firstCount = 0
+    let secondCount = 0
+    let result = '';
+
+    for(let i =0; i < goal.length; i++){
+        if(goal[i] === cards1[firstCount]){
+            result = "Yes";
+            firstCount += 1;
+        }else if(goal[i] === cards2[secondCount]){
+           result = "Yes"
+            secondCount += 1
+        }else{
+            result = "No";
+            break;
+        }  
     }
-    return "Yes";
+    return result;
 }
