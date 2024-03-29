2aca7c26a176263ee4279f6365817640d1ed9b36
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-29 20:08:08 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+// solution #1
+const solution = (cards1, cards2, goal) => {
+    for(const s of goal) {
+        if(cards1[0] === s) {
+            cards1.shift();
+        } else if(cards2[0] === s) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+    return "Yes";
+}
