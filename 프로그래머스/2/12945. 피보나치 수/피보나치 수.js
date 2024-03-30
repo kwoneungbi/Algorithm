9b70acd81ff1355692671412ef225302d396c450
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-03-30 19:26:25 +0900

    [level 2] Title: 피보나치 수, Time: 4.65 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,24 @@
-const solution = (n) => {
-    let result = 0;
-    let f0 = 0
-    let f1 = 1;
+// solution #1
+// const solution = (n) => {
+//     let result = 0;
+//     let f0 = 0
+//     let f1 = 1;
     
-    for(let i = 2; i <= n; i++){
-        result = (f0 + f1) % 1234567;
-        f0 = f1;
-        f1 = result;
+//     for(let i = 2; i <= n; i++){
+//         result = (f0 + f1) % 1234567;
+//         f0 = f1;
+//         f1 = result;
+//     }
+//     return result;
+// }
+
+// solution #2
+const solution = (n) => {
+    const memo = [0, 1]; 
+    
+    for (let i = 2; i <= n; i++) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
     }
-    return result;
-}
+    
+    return memo[n];
+};
