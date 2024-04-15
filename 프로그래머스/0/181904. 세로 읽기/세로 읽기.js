08ac1841992c305f3e19629c445772e86c9eae1d
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-04-15 17:50:10 +0900

    [level 0] Title: 세로 읽기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,9 @@
+const solution = (my_string, m, c) =>  {
+    let result ='';
+  
+    for(let i = c-1; i < my_string.length; i += m){
+    result += my_string[i];
+    }
+    
+    return result;
+}
