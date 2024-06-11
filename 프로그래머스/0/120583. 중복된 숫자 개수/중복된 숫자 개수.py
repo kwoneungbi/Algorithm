4a40e265ba5734b7e094349fefe2ee89023765ad
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-06-11 09:59:37 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -0,0 +1,9 @@
+# def solution(array, n):
+#     result = 0
+#     for x in array:
+#         if x == n:
+#             result += 1
+#     return result
+
+def solution(array, n):
+    return array.count(n)
