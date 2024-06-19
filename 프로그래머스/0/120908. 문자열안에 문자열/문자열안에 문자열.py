d8d54ad40ae703d3267833370841b97d0dc6f4da
eunbi <139939872+kwoneungbi@users.noreply.github.com>
2024-06-19 17:35:51 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,4 +1,9 @@
+# solution 1
+# def solution(str1, str2):
+#     if str1.find(str2) == -1:
+#         return 2
+#     else : return 1
+
+# solution 2
 def solution(str1, str2):
-    if str1.find(str2) == -1:
-        return 2
-    else : return 1
+    return 1 if str2 in str1 else 2
